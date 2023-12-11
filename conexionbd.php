77ff95d9b8109c90f04a022ffd6e20afe4c18a2c
DiegoSena <alefrito@554mail.com>
2023-12-11 06:59:00 -0500

    arreglando archivo

--- conexionbd.php
+++ conexionbd.php
@@ -1,14 +1,4 @@
-<?php
-session_start();
 
-if (!isset($_SESSION['rol'])) {
-    header('location: login.php');
-} else {
-    if ($_SESSION['rol'] != 1) {
-        header('location: login.php');
-    }
-}
-?>
 <?php
 
 class Database{
